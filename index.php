7520aacd9b14f7dea24872988d9a9c0d4f39f7eb
miah <86537561+mhmh4@users.noreply.github.com>
2022-08-08 08:55:12 -0400

    Refactor `index.php`
    Removed unnecessary code within `index.php` and add a link to `create.php` within it.

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <!doctype html>
 <html>
 <head>
@@ -10,48 +8,30 @@
     <title>Philoi with friends</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/style.css">
     <style>
-        button {
-            width: 100%;
-            height: 57px;
-        }
+        button { width: 100%; height: 57px; }
     </style>
 </head>
 <body>
-    <div class="d-flex" id="home">
-        <!-- Left pane -->
-        <?php include 'includes/shapes.php' ?> 
-
-
- 
-        <!-- Right pane -->
+    <div class="d-flex">
+        <?php // include 'includes/shapes.php' ?>
         <div class="d-flex flex-fill" style="background: #fff; width: 19rem">
-            <div class="d-flex flex-column mx-auto" style="width: 415px; margin-top: 80px">
-
+            <div class="d-flex flex-column mx-auto" style="width: 360px; margin-top: 80px">
                 <h1 class="text-center" style="color:#006480;"><strong>Welcome Philoi</strong></h1>
-                <h5 class="text-muted text-center mb-1">The place to check how much your friends know U </h5>
-                <!-- <h7 class="text-muted text-center mb-4"> The place to check how much your friends know U</h7> -->
-
-
-                <!-- <h7 class="text-muted text-center mb-4">The place to check how much you know your friends </h7> -->
-
+                <h5 class="text-muted text-center mb-1">The place to check how much your friends know you</h5>
                 <hr>
-                <div>
-                    <button class="btn btn-primary my-2 mb-1 user-btn" style="background: default" onclick="location = 'pin.php'">Join existing game</button>
-                </div>
-                <div>
-                    <button class="btn btn-success mt-1 staff-btn" style="background: default" onclick="location = 'new_game.php'">Make a new game</button>
-                </div>
+                <button class="btn btn-primary my-2 mb-1" onclick="location.href = 'pin.php'">Join existing game</button>
+                <button class="btn btn-secondary my-2" onclick="location.href = 'new_game.php'">Make a new game</button>
+                <button class="btn btn-success mt-1" onclick="location.href = 'create.php'">Create a new quiz</button>
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap JS with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
