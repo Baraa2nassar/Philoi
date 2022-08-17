1cd41c868a9fc625a34539bf4b2ffc9fb26d34a2
miah <86537561+mhmh4@users.noreply.github.com>
2022-08-17 07:31:16 -0400

    Fix link to renamed file (create1.php)

--- index.php
+++ index.php
@@ -43,21 +43,21 @@
     <div class="d-flex">
         <?php include 'includes/shapes.php' ?>
         <div class="d-flex flex-fill">
-            <div class="d-flex flex-column mx-auto" style="width: 350px; margin-top: 80px">
+            <div class="d-flex flex-column mx-auto" style="width: 350px; margin-top: 70px">
                 <h1 class="text-center" style="color:#006480;">Welcome to Philoi</h1>
                 <h5 class="text-center mx-1" style="color: cornflowerblue;">A game to see how much your friends know you</h5>
                 <hr>
                 <button class="btn btn-primary my-2 mb-1" onclick="location.href = 'join.php'">Join existing game</button>
-                <button class="btn btn-success my-2" onclick="location.href = 'new_game.php'">Make a new game</button>
+                <button class="btn btn-success my-2" onclick="location.href = 'create1.php'">Make a new game</button>
                 <button class="btn mt-1 text-white" id="rules-btn" onclick="location.href = 'rules.php'">View rules</button>
-  
+
             </div>
         </div>
     </div>
 
     <!-- Bootstrap JS with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
-        
+
 
 
     </script>
