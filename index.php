548c773a682166f6d24f18e3d2bb641ea1a9af8b
miah <86537561+mhmh4@users.noreply.github.com>
2022-08-17 23:05:39 -0400

    Beginning of redesign

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php require 'scripts/functions.php'; ?>
 <!doctype html>
 <html>
 <head>
@@ -6,73 +5,35 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Philoi with friends</title>
+    <title>Philoi</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/style.css">
     <style>
         button { width: 100%; height: 57px; }
         #rules-btn { background-color: #315659; border: 1px solid #315659 }
         #rules-btn:hover { background-color: #264042 }
-            body {
-      padding: 25px;
-      background-color: white;
-      color: black;
-      font-size: 25px;
-    }
-    .dark-mode {
-      background-color: black;
-      color: white;
-    }
-    .light-mode {
-      background-color: white;
-      color: black;
-    }
+        /* body { background-color: #2c3e50; } */
     </style>
 </head>
-
 <body>
+    <?php include 'includes/shapes.php' ?>
 
-    <!-- <h3 id="DarkModetext">Dark Mode is OFF</h3> -->
+    <div class="d-flex flex-fill">
+        <div class="d-flex flex-column mx-auto" style="width: 350px; margin-top: 80px">
+            <h1 class="text-center" style="color:#006480;">Welcome to Philoi</h1>
+            <h5 class="text-center mx-1" style="color: cornflowerblue;">A game to see how much your friends know you</h5>
+            <hr>
+            <button class="btn btn-primary my-2 mb-1" onclick="location.href = 'join.php'">Join existing game</button>
+            <button class="btn btn-success my-2" onclick="location.href = 'create1.php'">Make a new game</button>
+            <button class="btn mt-1 text-white" id="rules-btn" onclick="location.href = 'rules.php'">View rules</button>
 
-
-    <div class="d-flex">
-        <?php include 'includes/shapes.php' ?>
-        <div class="d-flex flex-fill">
-            <div class="d-flex flex-column mx-auto" style="width: 350px; margin-top: 70px">
-                <h1 class="text-center" style="color:#006480;">Welcome to Philoi</h1>
-                <h5 class="text-center mx-1" style="color: cornflowerblue;">A game to see how much your friends know you</h5>
-                <hr>
-                <button class="btn btn-primary my-2 mb-1" onclick="location.href = 'join.php'">Join existing game</button>
-                <button class="btn btn-success my-2" onclick="location.href = 'create1.php'">Make a new game</button>
-                <button class="btn mt-1 text-white" id="rules-btn" onclick="location.href = 'rules.php'">View rules</button>
-
-            </div>
         </div>
     </div>
 
     <!-- Bootstrap JS with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
-
-
-
-    </script>
-    <script>function darkMode() {
-    var element = document.body;
-    var content = document.getElementById("DarkModetext");
-    element.className = "dark-mode";
-    content.innerText = "Dark Mode is ON";
-}
-function lightMode() {
-    var element = document.body;
-    var content = document.getElementById("DarkModetext");
-    element.className = "light-mode";
-    content.innerText = "Dark Mode is OFF";
-}</script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
