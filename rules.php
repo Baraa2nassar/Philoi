a659575e5fba546b9b4a2fabfaad1a23b2d1e5f3
miah <86537561+mhmh4@users.noreply.github.com>
2022-08-19 18:39:06 -0400

    Update rules.php

--- rules.php
+++ rules.php
@@ -9,26 +9,21 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="d-flex">
-        <?php include 'includes/shapes.php' ?>
-        <div class="d-flex flex-fill">
-            <div class="d-flex flex-column mx-auto" style="width: 550px; margin-top: 70px">
-                <h1 class="text-center" style="color:#006480;">Rules of Philoi</h1>
-                <hr>
-                <p><strong>Philoi</strong> is a quiz game designed to test how well friends know each other.</p>
-                <p>It works by an organizer, a person who will serve as a game host, creating an equal amount of questions for a group of 2-5 friends each.</p>
-                <p>Once the quiz has been created, the organizer can start the game by visiting the join existing game link and entering the game code provided after creating a quiz.</p>
-                <p>After waiting in the lobby and having start the game, a random question from the quiz will be displayed. Once all players have read the question, it is the organizer's job to input the answers each player chooses.</p>
-                <p>After submitting their answers, the correct answer is displayed and the process repeats again until all questions are completed.</p>
+    <?php include 'includes/shapes.php' ?>
 
-                <button class="mx-auto btn btn-secondary mt-2" style="width: 150px" onclick="location.href = 'index.php'">Back</button>
-            </div>
+    <div class="d-flex flex-fill">
+        <div class="d-flex flex-column mx-auto" style="width: 550px; margin-top: 60px">
+            <h1 class="text-center" style="color:#006480;">Rules of Philoi</h1>
+            <hr>
+            <p>Philoi is a quiz game designed to test how well friends know each other.</p>
+            <p>It works by an organizer, a person who will serve as a game host, creating an equal amount of questions for a group of 2-5 friends each.</p>
+            <p>Once the quiz has been created, the organizer can start the game by visiting the join existing game link and entering the game code provided after creating a quiz.</p>
+            <p>After waiting in the lobby and having start the game, a random question from the quiz will be displayed. Once all players have read the question, it is the organizer's job to input the answers each player chooses.</p>
+            <p>After submitting their answers, the correct answer will be shown and the process repeats again until all questions have been completed.</p>
+
+            <button class="mx-auto btn btn-secondary mt-3" style="width: 150px" onclick="location.href = 'index.php'">Back</button>
         </div>
     </div>
 
