bbf31c978be98f145d98f80bbb8e0fd63e70ec1c
miah <86537561+mhmh4@users.noreply.github.com>
2022-08-20 16:46:52 -0400

    Move form to join a lobby to index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+
+session_start();
+
+$game_code = $_POST['game_code'] ?? null;
+
+if (isset($game_code)) {
+    require 'scripts/functions.php';
+    $pdo = get_database_connection();
+
+    $query = "SELECT * FROM quizzes WHERE quiz_id = ?";
+    $statement = $pdo->prepare($query);
+    $statement->execute(array($game_code));
+
+    if ($statement->rowCount() > 0) {
+        $_SESSION['game_code'] = $game_code;
+        header('Location: lobby.php');
+    } else {
+        $_SESSION["BAD_GAME_CODE"] = "Invalid game code. Please try again.";
+    }
+
+}
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -12,25 +36,28 @@
 
     <!-- Custom CSS -->
     <style>
-        button { width: 100%; height: 57px; }
-        #rules-btn { background-color: #315659; border: 1px solid #315659 }
-        #rules-btn:hover { background-color: #264042 }
         /* body { background-color: #4186e8; } */
     </style>
 </head>
 <body>
     <?php include 'includes/shapes.php' ?>
 
-    <div class="d-flex flex-fill">
-        <div class="d-flex flex-column mx-auto" style="width: 350px; margin-top: 80px">
-            <h1 class="text-center" style="color:#006480;">Welcome to Philoi</h1>
-            <h5 class="text-center mx-1" style="color: cornflowerblue;">A game to see how much your friends know you</h5>
-            <hr>
-            <button class="btn btn-primary my-2 mb-1" onclick="location.href = 'join.php'">Join existing game</button>
-            <button class="btn btn-success my-2" onclick="location.href = 'create1.php'">Make a new game</button>
-            <button class="btn mt-1 text-white" id="rules-btn" onclick="location.href = 'rules.php'">View rules</button>
+    <div class="d-flex flex-column mx-auto" style="width: 360px; margin-top: 90px">
+        <h1 class="text-center" style="color:#006480;">Welcome to Philoi</h1>
+        <h5 class="text-center mx-1" style="color: cornflowerblue;">A game to see how much your friends know you</h5>
+        <hr>
 
-        </div>
+        <form class="row my-2 mb-3" method="post">
+            <div class="col-8">
+                <input class="form-control bg-light" type="text" name="game_code" placeholder="Enter your game code" required>
+            </div>
+            <div class="col-4 text-center">
+                <button class="btn btn-secondary" style="width: 100%">Join</button>
+            </div>
+        </form>
+
+        <button class="btn btn-primary my-1" style="height: 57px" onclick="location.href = 'create1.php'">Make a new game</button>
+        <button class="btn btn-success my-1" style="height: 57px" onclick="location.href = 'rules.php'">View rules</button>
     </div>
 
     <!-- Bootstrap JS with Popper -->
