88d540c07be45aec47eeecc1406eb3ab5c5c4d59
miah <86537561+mhmh4@users.noreply.github.com>
2022-08-29 11:07:40 -0400

    Add SQL Dump file

---
+++ philoi.sql
@@ -0,0 +1,17 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `quizzes` (
+  `quiz_id` int(11) NOT NULL,
+  `questions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`questions`)),
+  `choices` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`choices`)),
+  `answers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`answers`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `quizzes`
+  ADD PRIMARY KEY (`quiz_id`);
+
+ALTER TABLE `quizzes`
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
