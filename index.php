cf9a6065e0fd208d8bf02ab7f41cc0f45cec614d
Baraa Nassar <baraa2aziz@gmail.com>
2022-11-15 23:24:02 -0500

    background image for index.php

--- index.php
+++ index.php
@@ -27,6 +27,8 @@ if (isset($game_code)) {
 <!doctype html>
 <html lang="en">
 <head>
+    <link rel="icon" href="https://camo.githubusercontent.com/c11671f6642668cc8677e0ecf16c75406bb86d4262d75ba2fad480290509c999/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f373432302f373432303639302e706e67">
+
     <!-- Meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -35,10 +37,11 @@ if (isset($game_code)) {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-</head>
-<body class="p-3 mb-2 bg-dark text-white">
-    <div class="container">
+    <link rel="stylesheet" href="css/styles.css">
 
+</head>
+<body class="hero">
+    <!-- <div class = ""> -->
     <div class="d-flex flex-column mx-auto" style="width: 360px; margin-top: 90px">
         <h1 class="text-center" style="color: DarkTurquoise    ;">Welcome to Philoi</h1>
         <h6 class="text-center mx-1" style="color: lightgray;">A game to see how much your friends know you</h6>
@@ -64,7 +67,7 @@ if (isset($game_code)) {
         <button class="btn btn-primary my-1" style="height: 57px" onclick="location.href = 'create1.php'">Make a new game</button>
         <button class="btn btn-success mt-2" style="height: 57px" onclick="location.href = 'rules.php'">View rules</button>
     </div>
-
+<!-- </div> -->
     <!-- Bootstrap JS with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
