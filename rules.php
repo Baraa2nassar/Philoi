5c819163d53021fc7c8f0dccab04851ff45b5d2d
miah <86537561+mhmh4@users.noreply.github.com>
2022-11-22 20:58:46 -0500

    Refactored rules page

--- rules.php
+++ rules.php
@@ -12,56 +12,31 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="css/styles.css">
-
 </head>
-<body class="p-3 mb-2 bg-dark text-white">
-    <div class="d-flex flex-column mx-auto" style="width: 600px; margin-top: 60px">
+<body class="p-3 mb-2 text-white" style="background-color: #16202c;">
+    <div class="d-flex flex-column mx-auto" style="width: 600px; margin-top: 40px">
 
-        <section class="tm-site-header tm-flex-center tm-mb-50 tm-bgcolor-1 tm-border-rounded">
+        <section class="my-2 p-2 text-center" style="background-color: #458A45; border-radius: 12px;">
+            <h1 class="mt-2">What is Philoi? 👀</h1>
+            <p class="text-center">Philoi is a quiz game designed to test how well friends know each other.</p>
+        </section>
 
-        <div class="split left">
-            <i class="fas fa-heart fa-3x"></i>
-            <h1> What is Philoi?👀</h1>
+        <section class="my-2 p-2 text-center" style="background-color: #A63F3C; border-radius: 12px;">
+            <h1 class="mt-2">Organizer 👑</h1>
+            <p class="text-center">One person will serve as the organizer. They will create the questions for a set of players, host the game, and input the answer each player gives.</p>
+        </section>
+
+        <section class="my-2 p-2 text-center" style="background-color: #438DA3; border-radius: 12px;">
+            <h1 class="mt-2">Players 🕹️</h1>
+            <p class="text-center">Players can see the game by the organizer sharing their screen with them or the players can be together in a physical location.</shadow>
+        </section>
+
+        <div class="col text-center">
+            <button class="mx-auto btn btn-secondary mt-3" style="width: 200px" onclick="location.href = 'index.php'">Back</button>
         </div>
-
-        <!-- <div class="split right"> -->
-            <shadow class="text-center">Philoi is a quiz game designed to test how well friends know each other</shadow>
-        <!-- </div>   -->
-
-        </section>
-    <div class="column">
-
-
-
-         <section class="tm-site-header tm-flex-center tm-mb-50 tm-bgcolor-2 tm-border-rounded">
-            <i class="fas fa-heart fa-3x"></i>
-
-            <h1> Organizer 👑</h1>
-
-            <p class="text-center">One person will serve as the organizer. </p>
-            Organizer creates the set of players questions
-            <br>Organizer hosts the game.
-            <br>Organizer's job to input each answer the players give.
-            <!-- <p></p> -->
-            <!-- </p class="text-center"> -->
-
-
-        </section>
-        <section class="tm-site-header tm-flex-center tm-mb-50 tm-bgcolor-3 tm-border-rounded">
-            <i class="fas fa-heart fa-3x"></i>
-            <h1> Players♟</h1>
-            <shadow class="text-center">Players can see the game, the organizer can either share their screen with everyone or the players can be together in a physical location.</shadow>
-        </section>
-
-
-        <!-- <p>Philoi is a quiz game designed to test how well friends know each other.</p> -->
-    <div class="col text-center">
-
-        <button class="mx-auto btn btn-secondary mt-3" style="width: 160px" onclick="location.href = 'index.php'">Back</button>
     </div>
-    </div>
-
 
     <!-- Bootstrap JS with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
