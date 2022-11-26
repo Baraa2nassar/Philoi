bfdeaf210e105f0ca7f24b2338b052dd5ce10651
Baraa Nassar <baraa2aziz@gmail.com>
2022-11-26 02:39:33 -0500

    environment variables implemented

--- includes/functions.php
+++ includes/functions.php
@@ -2,12 +2,13 @@
 
 function get_database_connection()
 {
-    $dsn = "mysql:dbname=philoi;host=localhost";
-    $username = "root";
-    $password = "";
+    $dsn = $_SERVER['API_ENDPOINT'];
+    $username =  $_SERVER['USERNAME'];
+    $password =  $_SERVER['PASS'];
 
     try {
         $pdo = new PDO($dsn, $username, $password);
+        // $pdo = new mysqli($host, $username, $password,$db_name);
     }
     catch (PDOException $exception) {
         $error_message = $exception->getMessage();
