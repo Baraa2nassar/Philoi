1ceb3ca6b4a50d0623c2c546f2d9609dfeec3cfb
miah <86537561+mhmh4@users.noreply.github.com>
2022-12-21 23:57:08 -0500

    Update rules.php

--- rules.php
+++ rules.php
@@ -16,18 +16,18 @@
 <body class="bg-dark">
     <div class="d-flex flex-column mx-auto mt-5" style="width: 600px;">
 
-        <section class="my-2 p-2 px-3" style="background-color: #14181d; border-radius: 12px;">
-            <h1 class="mt-2 fs-3" style="color: #458A45">What is Philoi? 🔎</h1>
+        <section class="my-2 p-2 px-3" style="background-color: #286279; border-radius: 12px;">
+            <h1 class="mt-2 fs-3 text-white">What is Philoi? 🔎</h1>
             <p class="text-light ">Philoi is a quiz game designed to test how well friends know each other.</p>
         </section>
 
-        <section class="my-2 p-2 px-3" style="background-color: #14181d; border-radius: 12px;">
-            <h1 class="mt-2 fs-3" style="color: #A63F3C">Organizer 🧠</h1>
+        <section class="my-2 p-2 px-3" style="background-color: #286279; border-radius: 12px;">
+            <h1 class="mt-2 fs-3 text-white">Organizer 🧠</h1>
             <p class="text-light ">One person will serve as the organizer. They will create the questions for a set of players, host the game, and input the answer each player gives.</p>
         </section>
 
-        <section class="my-2 p-2 px-3" style="background-color: #14181d; border-radius: 12px;">
-            <h1 class="mt-2 fs-3" style="color: #438DA3">Players 🕹️</h1>
+        <section class="my-2 p-2 px-3" style="background-color: #286279; border-radius: 12px;">
+            <h1 class="mt-2 fs-3 text-white">Players 🕹️</h1>
             <p class="text-light ">Players can see the game by the organizer sharing their screen with them or the players can be together in a physical location.</shadow>
         </section>
 
