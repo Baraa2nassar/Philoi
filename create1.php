e2b6e44737bd0447566789d7941d3fe49afd8e5d
miah <86537561+mhmh4@users.noreply.github.com>
2022-12-23 12:50:14 -0500

    Update create1.php

--- create1.php
+++ create1.php
@@ -46,7 +46,7 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
 <body class="bg-dark">
-    <div class="d-flex flex-column mx-auto text-white" style="width: 420px; margin-top: 40px">
+    <div class="d-flex flex-column mx-auto text-white" style="width: 420px; margin-top: 3%">
         <h1 class="text-center" style="color: darkturquoise">Create a New Game</h1>
         <hr>
 
@@ -63,11 +63,11 @@ if (isset($_POST['submit'])) {
                 <h5 class="text-center mb-1" style="color: lightblue;">Enter the names of your friends <span class="" style="opacity: 70%">(3 min)</span></h5>
 
                 <!-- Player names -->
-                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 1" style="color: #565656; border-width: 2.5px; border-color: #5a5b5c">
-                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 2" style="color: #d14430; border-width: 2.5px; border-color: #d92007">
-                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 3" style="color: #28a745; border-width: 2.5px; border-color: #28a745">
-                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 4" style="color: #885cb5; border-width: 2.5px; border-color: #9b42f5">
-                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 5" style="color: #449DD1; border-width: 2.5px; border-color: #449DD1">
+                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 1" style="border-width: 2.5px">
+                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 2" style="border-width: 2.5px">
+                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 3" style="border-width: 2.5px">
+                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 4" style="border-width: 2.5px">
+                <input class="form-control my-3" type="text" name="players[]" id="username" placeholder="Player 5" style="border-width: 2.5px">
 
                 <?php if (isset($_SESSION['INVALID_PLAYER_NAMES'])): ?>
                     <div class="alert alert-danger text-center" style="padding: 10px 20px">
@@ -77,9 +77,11 @@ if (isset($_POST['submit'])) {
                     <?php unset($_SESSION['INVALID_PLAYER_NAMES']); ?>
                 <?php endif; ?>
 
-                <div class="d-flex">
-                    <button class="mx-3 btn btn-secondary mt-1" type="button" style="width: 225px" onclick="location.href = 'index.php'">Back</button>
-                    <button class="mx-3 btn btn-primary mt-1" name="submit" value="submit" style="width: 225px">Continue</button>
+                <div class="d-flex mt-4">
+                    <a href="index.php" tabindex="-1">
+                        <button class="me-2 btn btn-secondary" type="button" style="width: 200px;">Back</button>
+                    </a>
+                    <button class="ms-2 btn btn-primary" name="submit" style="width: 200px">Continue</button>
                 </div>
             </div>
         </form>
