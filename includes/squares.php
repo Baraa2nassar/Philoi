78afab1e8d0f087528ac429badd7476349190c0e
miah <86537561+mhmh4@users.noreply.github.com>
2022-12-31 00:13:00 -0500

    Add more □'s

--- includes/squares.php
+++ includes/squares.php
@@ -15,10 +15,12 @@
 <div>
     <!-- Left side -->
     <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top:  30px; left: 150px;"></div>
+    <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 350px; left: -50px;"></div>
     <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 200px; left:  90px;"></div>
     <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 400px; left: 200px;"></div>
     <!-- Right side -->
     <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: -10px; right: 200px;"></div>
+    <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 100px; right:  30px;"></div>
     <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 250px; right: 210px;"></div>
-    <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 400px; right: 100px;"></div>
+    <div class="square" style="height: 150px; width: 150px; rotate: 30deg; top: 430px; right:  60px;"></div>
 </div>
