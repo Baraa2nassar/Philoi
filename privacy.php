5f445c2a310e29353eb4c0d2f9326c714e507cae
miah <86537561+mhmh4@users.noreply.github.com>
2023-01-03 14:34:18 -0500

    Create privacy.php

---
+++ privacy.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Privacy Policy - Philoi</title>
+
+    <!-- Favorite icon -->
+    <link rel="icon" href="static/images/favicon.ico" type="image/x-icon">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="static/css/other.css">
+
+    <style>
+        @media screen and (max-width: 550px) { body div { width: 28rem !important; } }
+        @media screen and (max-width: 500px) { body div { width: 26rem !important; } }
+        @media screen and (max-width: 450px) { body div { width: 23rem !important; } }
+        @media screen and (max-width: 400px) { body div { width: 20rem !important; } }
+        @media screen and (max-width: 350px) { body div { width: 18rem !important; } }
+    </style>
+</head>
+<body>
+    <div class="d-flex flex-column mx-auto" style="width: 30rem; margin-top: 3%;">
+        <h1 class="text-center" style="color: lightblue;">Privacy Policy</h1>
+        <hr class="text-white">
+        <div class="bg-light mx-auto" style="max-height: 380px; overflow-y: auto">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque optio debitis quaerat officiis quibusdam fugiat, ullam omnis quidem provident ipsa deserunt a sunt amet est explicabo veritatis! Non, et impedit.
+            Vel consequuntur harum recusandae, error quaerat dignissimos sed! Doloribus tenetur at modi error soluta, iusto sit. Numquam repudiandae aut commodi maxime eaque voluptatum possimus sapiente, ducimus, quaerat nesciunt voluptas a.
+            Vel nisi nostrum voluptate temporibus quia veniam in nam quisquam sit, quibusdam illo voluptas debitis. Totam magnam officia nulla architecto voluptatum? Amet delectus dolorem sed error aperiam facilis quibusdam saepe.
+            Ipsum eum dolorum sunt ea illum nihil sit hic iure totam dolores laudantium maiores assumenda laboriosam rem quia, eligendi veritatis facilis unde repudiandae ab eos sed? Ad magnam tempora recusandae.
+            Nemo, repudiandae autem laborum ea enim facilis itaque. Distinctio sapiente magnam aperiam quasi! Vel sit veritatis, ducimus nisi porro quaerat, unde, quo deleniti enim facilis dolorum praesentium labore libero. Esse.
+            Quia, sunt optio iure iste illum odio totam omnis, distinctio esse, est cupiditate. Eos praesentium maiores ab id quas? Dolore, ullam illum non iusto voluptates rem iure amet voluptatibus nemo?
+            Facilis architecto molestias dolores dolor quae ipsam molestiae doloribus, inventore voluptatibus, at omnis ducimus? Voluptate enim tempore numquam sequi hic facilis, repellendus error architecto id impedit, magnam optio expedita perspiciatis.
+            Aliquid non quasi iusto, illum fuga reprehenderit sequi tenetur, temporibus perspiciatis odio blanditiis quis commodi quidem. Fugiat sunt explicabo libero perspiciatis sequi, corrupti itaque id eaque tempore consequuntur error saepe!
+            Sit, maxime nemo voluptates cum repellendus atque? Quae sequi necessitatibus, in repellat tenetur cupiditate reiciendis distinctio laborum libero a atque beatae sint earum quam ullam repudiandae possimus? Hic, voluptatum facilis?
+            Voluptatem, molestiae nobis perferendis nostrum enim accusantium repellat libero dolore adipisci blanditiis itaque dolorem dicta ducimus labore tenetur voluptates. Nesciunt dicta minus laborum dolore tempore non voluptas impedit repudiandae nobis!
+        </div>
+
+        <div class="col text-center">
+            <button class="mx-auto btn btn-secondary custom-btn-secondary mt-4" style="width: 200px" onclick="location.href = 'index.php'">Back</button>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
