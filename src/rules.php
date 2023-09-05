b489fb45494e63169e30e141a37ddb31eceed2f1
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-05 15:49:16 -0400

    Update rules.php

--- src/rules.php
+++ src/rules.php
@@ -1,43 +1,32 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Rules - Philoi</title>
+  <title>Rules - Philoi</title>
 
-    <!-- Favorite icon -->
-    <link rel="icon" href="static/images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="static/images/favicon.ico">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="static/css/other.css">
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<style>
-        @media screen and (max-width: 550px) { #rules { width: 28rem !important; } }
-        @media screen and (max-width: 500px) { #rules { width: 26rem !important; } }
-        @media screen and (max-width: 450px) { #rules { width: 23rem !important; } }
-        @media screen and (max-width: 400px) { #rules { width: 20rem !important; } }
-        @media screen and (max-width: 350px) { #rules { width: 18rem !important; } }
-    </style>
-    <div class="d-flex flex-column mx-auto mt-5" id="rules" style="width: 30rem;">
-        <section class="my-2 p-2 px-3" style="background-color: #286279; border-radius: 12px;">
-            <h1 class="mt-2 fs-3 text-white">What is Philoi? &#128269;</h1>
-            <p class="text-light ">Philoi is a quiz game designed to test how well friends know each other.</p>
-        </section>
-        <section class="my-2 p-2 px-3" style="background-color: #286279; border-radius: 12px;">
-            <h1 class="mt-2 fs-3 text-white">Organizer &#129504;</h1>
-            <p class="text-light ">One person will serve as the organizer. They will create the questions for a set of players, host the game, and input the answer each player gives.</p>
-        </section>
-        <section class="my-2 p-2 px-3" style="background-color: #286279; border-radius: 12px;">
-            <h1 class="mt-2 fs-3 text-white">Players &#128377;</h1>
-            <p class="text-light ">Players can see the game by the organizer sharing their screen with them or the players can be together in a physical location.</p>
-        </section>
-        <a href="index.php" class="col text-center">
-            <button class="mx-auto btn btn-secondary custom-btn-secondary mt-3" id="rules-back-button" style="width: 200px">Back</button>
-        </a>
-    </div>
+<body class="bg-[#1B4353]">
+  <div class="flex flex-col mx-auto mt-6 w-2/5 text-white">
+    <section class="my-2 p-3 bg-[#286279] rounded-xl">
+      <h1 class="text-2xl my-1">What is Philoi? &#128269;</h1>
+      <p>Philoi is a quiz game designed to test how well friends know each other.</p>
+    </section>
+    <section class="my-2 p-3 bg-[#286279] rounded-xl">
+      <h1 class="text-2xl my-1">Organizer &#129504;</h1>
+      <p class="text-white">One person will serve as the organizer. They will create the questions for a set of players, host the game, and input the answer each player gives.</p>
+    </section>
+    <section class="my-2 p-3 bg-[#286279] rounded-xl">
+      <h1 class="text-2xl my-1">Players &#128377;</h1>
+      <p class="text-white">Players can see the game by the organizer sharing their screen with them or the players can be together in a physical location.</p>
+    </section>
+    <a href="index.php" class="mt-5 mx-auto w-2/3 bg-[#5D737E] py-2 rounded-md text-center hover:bg-[#546771]">
+      <span class="">Back</span>
+    </a>
+  </div>
 </body>
 </html>
