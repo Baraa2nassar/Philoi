772b65cd2012967ef32ef3795f376501e62c98bd
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-06 22:03:27 -0400

    Partially transition to Tailwind

--- src/index.php
+++ src/index.php
@@ -55,78 +55,74 @@ if (isset($_POST['play_demo'])) {
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Philoi</title>
 
-    <!-- Favorite icon -->
     <link rel="icon" href="static/images/favicon.ico" type="image/x-icon">
 
-    <!-- Bootstrap CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="static/css/other.css">
     <style> footer div a:hover { text-decoration: underline !important; } </style>
 </head>
 <body>
     <?php include 'includes/squares.php'; ?>
-    <main id="main">
-        <div class="d-flex flex-column mx-auto" style="width: 360px; margin-top: 60px">
-            <h1 class="text-center" style="color: lightblue;">Philoi</h1>
-            <h6 class="text-center mx-1" style="color: lightgray;">A game to see how well friends know each other</h6>
-            <hr class="text-white">
+    <div class="d-flex flex-column mx-auto mt-14 w-1/3">
+        <h1 class="text-center text-4xl text-[#ADD8E6]">Philoi</h1>
+        <h6 class="text-center my-2 text-[#D3D3D3]">A game to see how well friends know each other</h6>
+        <hr class="text-white my-3">
 
-            <form class="row my-2 mb-3" method="post">
-                <div class="col-8">
-                    <input class="form-control" type="text" name="game_pin" placeholder="Enter a Game PIN" required>
+        <form class="row my-2 mb-3" method="post">
+            <div class="col-8">
+                <input class="form-control" type="text" name="game_pin" placeholder="Enter a Game PIN" required>
+            </div>
+            <div class="col-4 text-center">
+                <button class="btn custom-btn-success text-white w-100" style="background: #588157">Enter</button>
+            </div>
+
+        </form>
+
+        <?php if (isset($_SESSION['INVALID_GAME_PIN'])): ?>
+            <div class="alert alert-danger text-center" style="padding: 5px 20px">
+                <?= $_SESSION['INVALID_GAME_PIN'] ?>
+            </div>
+
+            <?php unset($_SESSION['INVALID_GAME_PIN']); ?>
+        <?php endif; ?>
+
+        <form method="post">
+            <div class="rounded text-center mb-2 py-1 demo">
+                <span class="">New to Philoi?<span> <button name="play_demo">Play a demo game</button></span>
+            </div>
+        </form>
+
+        <a href="create1.php" class="my-1" tabindex="-1">
+            <button class="btn custom-btn-primary text-white w-100" style="height: 57px;">Create a new quiz</button>
+        </a>
+        <a href="rules.php" class="mt-2" tabindex="-1">
+            <button class="btn custom-btn-primary text-white w-100" style="height: 57px;">View rules</button>
+        </a>
+
+        <footer class="d-flex flex-column text-center rounded mt-5">
+            <div class="d-flex">
+                <div class="m-auto mt-1">
+                    <a class="text-white" href="https://github.com/Baraa2nassar/Philoi/" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">View on GitHub</a>
                 </div>
-                <div class="col-4 text-center">
-                    <button class="btn custom-btn-success text-white w-100" style="background: #588157">Enter</button>
+                <div class="my-auto" style="user-select: none !important; color: #fff; opacity: 0.2;">|</div>
+                <div class="m-auto mt-1">
+                    <a class="text-white" href="mailto:baraa2aziz@gmail.com" target="_blank" style="text-decoration: none;">Contact Us</a>
                 </div>
-
-            </form>
-
-            <?php if (isset($_SESSION['INVALID_GAME_PIN'])): ?>
-                <div class="alert alert-danger text-center" style="padding: 5px 20px">
-                    <?= $_SESSION['INVALID_GAME_PIN'] ?>
+                <div class="my-auto" style="user-select: none !important; color: #fff; opacity: 0.2;">|</div>
+                <div class="m-auto mt-1">
+                    <a href="privacy.php" class="text-white" style="text-decoration: none; cursor: pointer;">Privacy Policy</a>
                 </div>
+            </div>
+        </footer>
+    </div>
 
-                <?php unset($_SESSION['INVALID_GAME_PIN']); ?>
-            <?php endif; ?>
-
-            <form method="post">
-                <div class="rounded text-center mb-2 py-1 demo">
-                    <span class="">New to Philoi?<span> <button name="play_demo">Play a demo game</button></span>
-                </div>
-            </form>
-
-            <a href="create1.php" class="my-1" tabindex="-1">
-                <button class="btn custom-btn-primary text-white w-100" style="height: 57px;">Create a new quiz</button>
-            </a>
-            <a href="rules.php" class="mt-2" tabindex="-1">
-                <button class="btn custom-btn-primary text-white w-100" style="height: 57px;">View rules</button>
-            </a>
-
-            <footer class="d-flex flex-column text-center rounded mt-5">
-                <div class="d-flex">
-                    <div class="m-auto mt-1">
-                        <a class="text-white" href="https://github.com/Baraa2nassar/Philoi/" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">View on GitHub</a>
-                    </div>
-                    <div class="my-auto" style="user-select: none !important; color: #fff; opacity: 0.2;">|</div>
-                    <div class="m-auto mt-1">
-                        <a class="text-white" href="mailto:baraa2aziz@gmail.com" target="_blank" style="text-decoration: none;">Contact Us</a>
-                    </div>
-                    <div class="my-auto" style="user-select: none !important; color: #fff; opacity: 0.2;">|</div>
-                    <div class="m-auto mt-1">
-                        <a href="privacy.php" class="text-white" style="text-decoration: none; cursor: pointer;">Privacy Policy</a>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </main>
-
-    <!-- Bootstrap JS with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
