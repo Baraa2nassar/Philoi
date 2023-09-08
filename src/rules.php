503a38c0a24acb6f14b1efb445fc1defa8ae5024
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-07 20:45:00 -0400

    Update rules.php

--- src/rules.php
+++ src/rules.php
@@ -7,26 +7,37 @@
   <title>Rules - Philoi</title>
 
   <link rel="icon" href="static/icons/favicon.ico">
-
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#1B4353]">
-  <div class="flex flex-col mx-auto mt-6 w-2/5 text-white">
-    <section class="my-2 p-3 bg-[#286279] rounded-xl">
+  <div class="flex flex-col mx-auto mt-6 w-1/3 text-white">
+    <section class="mb-3">
+      <h1 class="text-center text-4xl text-[#ADD8E6]">Rules</h1>
+      <hr class="h-px border-0 bg-slate-400 my-2">
+    </section>
+
+    <section class="mb-3 p-3 bg-[#286279] rounded-xl">
       <h1 class="text-2xl my-1">What is Philoi? &#128269;</h1>
       <p>Philoi is a quiz game designed to test how well friends know each other.</p>
     </section>
-    <section class="my-2 p-3 bg-[#286279] rounded-xl">
+
+    <section class="mb-3 p-3 bg-[#286279] rounded-xl">
       <h1 class="text-2xl my-1">Organizer &#129504;</h1>
-      <p class="text-white">One person will serve as the organizer. They will create the questions for a set of players, host the game, and input the answer each player gives.</p>
+      <p>
+        One person will serve as the organizer. They will create the questions for a
+        set of players, host the game, and input the answer each player gives.
+      </p>
     </section>
-    <section class="my-2 p-3 bg-[#286279] rounded-xl">
+
+    <section class="mb-3 p-3 bg-[#286279] rounded-xl">
       <h1 class="text-2xl my-1">Players &#128377;</h1>
-      <p class="text-white">Players can see the game by the organizer sharing their screen with them or the players can be together in a physical location.</p>
+      <p>
+        Players can view the game by the organizer sharing their
+        screen or by being in a physical location together.
+      </p>
     </section>
-    <a href="index.php" class="mt-5 mx-auto w-2/3 bg-[#5D737E] py-2 rounded-md text-center hover:bg-[#546771]">
-      <span class="">Back</span>
-    </a>
+
+    <a href="index.php" class="mt-4 mx-auto w-2/3 bg-[#5D737E] py-2 rounded-md text-center hover:bg-[#546771]">Back</a>
   </div>
 </body>
 </html>
