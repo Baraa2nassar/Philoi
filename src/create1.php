333dfa78d29d93fcfbe32f5c9bd5df9e0a4dc153
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-09 12:26:38 -0400

    Update create1.php

--- src/create1.php
+++ src/create1.php
@@ -49,47 +49,45 @@ if (isset($_POST['submit'])) {
 </head>
 <body class="bg-[#1B4353]">
 
-  <div class="w-1/3 mx-auto text-white mt-6">
+  <div class="w-1/3 mx-auto text-white mt-8">
     <section class="mb-3">
       <h1 class="text-center text-4xl text-[#ADD8E6]">Create a New Game</h1>
-      <hr class="h-px border-0 bg-slate-400 my-2">
+      <hr class="h-px border-0 bg-slate-400 my-3">
     </section>
 
         <form method="post">
             <div class="flex mb-3">
-                <label class="w-5/6 p-2 text-center col-sm-9 col-form-label">How many questions per player? <span style="color: #90A9B7">(10 max)</span></label>
-                <input class="w-1/6 p-2 text-black rounded" type="number" step="1" name="num_questions_per_player" min="1" max="10" required>
+                <label class="w-5/6 p-2 py-1">How many questions per player? <span style="color: #90A9B7">(10 max)</span></label>
+                <input class="w-1/6 ps-1.5 text-black rounded" type="number" step="1" name="num_questions_per_player" min="1" max="10" required>
             </div>
 
-            <div class="mt-3 text-center">
-                <h5 class="text-center mb-1" style="color: #efefef;">Enter the names of the players <span style="color: #90A9B7">(3 min)</span></h5>
+                <h5 class="text-center mb-3 text-xl" style="color: #efefef;">Enter the names of the players <span style="color: #90A9B7">(3 min)</span></h5>
 
-                <input class="my-3 block w-full p-2 rounded-md text-black" type="text" name="players[]" placeholder="Player 1" maxlength="17">
-                <input class="my-3 block w-full p-2 rounded-md text-black" type="text" name="players[]" placeholder="Player 2" maxlength="17">
-                <input class="my-3 block w-full p-2 rounded-md text-black" type="text" name="players[]" placeholder="Player 3" maxlength="17">
-                <input class="my-3 block w-full p-2 rounded-md text-black" type="text" name="players[]" placeholder="Player 4" maxlength="17">
+                <input class="mb-4 block w-full px-2.5 py-1.5 rounded-md text-black border border-2 border-gray-200 bg-[#f7f5f3]" type="text" name="players[]" placeholder="Player 1" maxlength="17">
+                <input class="mb-4 block w-full px-2.5 py-1.5 rounded-md text-black border border-2 border-gray-200 bg-[#f7f5f3]" type="text" name="players[]" placeholder="Player 2" maxlength="17">
+                <input class="mb-4 block w-full px-2.5 py-1.5 rounded-md text-black border border-2 border-gray-200 bg-[#f7f5f3]" type="text" name="players[]" placeholder="Player 3" maxlength="17">
+                <input class="mb-4 block w-full px-2.5 py-1.5 rounded-md text-black border border-2 border-gray-200 bg-[#f7f5f3]" type="text" name="players[]" placeholder="Player 4" maxlength="17">
 
                 <?php if (isset($_SESSION['ERROR'])): ?>
-                    <div class="alert alert-danger text-center" style="padding: 10px 5px">
+                    <div class="mb-3 text-[#963c44] bg-[#f8d7da] text-center py-2.5 p-5 rounded">
                         <?= $_SESSION['ERROR'] ?>
                     </div>
 
                     <?php unset($_SESSION['ERROR']); ?>
                 <?php endif; ?>
 
-                <section>
+                <section class="text-center mt-3">
                         <button name="submit" class="mb-3 rounded-md px-2 py-2 bg-[#1D70A2] border-[#1D70A2] hover:bg-[#185f89] hover:border-[#185f89] border w-2/3 mx-auto">
                             Continue
                         </button>
 
                     <a href="index.php" class="m">
-                        <div class="mx-auto w-2/3 bg-[#5D737E] py-2 rounded-md text-center hover:bg-[#546771]">
+                        <div class="mx-auto w-2/3 bg-[#5D737E] py-2 rounded-md text-center hover:bg-[#546771] select-none">
                         Back
                         </div>
                     </a>
                 </section>
 
-            </div>
         </form>
     </div>
 
