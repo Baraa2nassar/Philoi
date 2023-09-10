3e952f08084269becfc3430795805fd5871ac252
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-10 10:58:48 -0400

    Update create2.php

--- src/create2.php
+++ src/create2.php
@@ -70,60 +70,59 @@ $ordinal = [null, "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th",
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Create a new game - Philoi</title>
+  <title>Create a new game - Philoi</title>
 
-    <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="icon" href="static/icons/favicon.ico">
 
-    <!-- Favorite icon -->
-    <link rel="icon" href="static/icons/favicon.ico" type="image/x-icon">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="static/css/other.css">
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#1B4353]">
+  <?php include "includes/squares.php" ?>
 
   <div class="w-1/3 mx-auto text-white mt-8">
 
   <section class="mb-3">
       <h1 class="text-center text-4xl text-[#ADD8E6]">Create a New Game</h1>
-      <hr class="h-px border-0 bg-[#395b6c] my-3">
-    </section>
+      <hr class="h-px border-0 bg-[#395b6c] my-4">
+  </section>
 
         <form method="post">
-            <h5 class="mb-3 text-center text-white" style="color: #000;">Enter the questions for each player</h5>
+            <h5 class="mb-3 text-center text-white">Enter the questions for each player</h5>
                 <div class="rounded" style="max-height: 350px; overflow-y: auto;">
-                    <div class="">
-                        <div class="my-2">
-                            <?php for ($i = 0; $i < $num_players; $i++): ?>
-                                <section class="my-3 border p-2 rounded">
-                                    <?= $players[$i] ?>
-                                    <?php for ($j = 1; $j <= $num_questions_per_player; $j++): ?>
-                                        <div class="row mx-2 my-1">
-                                            <input class="block w-full px-2.5 py-1.5 rounded-md text-black border border-2 border-gray-200 bg-[#f7f5f3]" name="questions[]" type='text' placeholder='Enter the <?= $ordinal[$j]; ?> question for <?=$players[$i]?>' required>
-                                        </div>
-                                    <?php endfor; ?>
-                                </section>
-
+                    <?php for ($i = 0; $i < $num_players; $i++): ?>
+                        <section class="mb-3 border p-2 rounded">
+                            <?= $players[$i] ?>
+                            <?php for ($j = 1; $j <= $num_questions_per_player; $j++): ?>
+                                <div class="row mx-2 my-1">
+                                    <input class="block w-full px-2.5 py-1.5 rounded-md text-black border border-2 border-gray-200 bg-[#f7f5f3]" name="questions[]" type='text' placeholder='Enter the <?= $ordinal[$j]; ?> question for <?=$players[$i]?>' required>
+                                </div>
                             <?php endfor; ?>
-                        </div>
-                        <?php if (isset($_SESSION['INVALID_QUESTION'])): ?>
-                            <div class="alert alert-danger text-center mt-3" style="padding: 10px 20px">
-                                <?= $_SESSION['INVALID_QUESTION'] ?>
-                            </div>
+                        </section>
 
-                            <?php unset($_SESSION['INVALID_QUESTION']); ?>
-                        <?php endif; ?>
+                    <?php endfor; ?>
+                <?php if (isset($_SESSION['INVALID_QUESTION'])): ?>
+                    <div class="alert alert-danger text-center mt-3" style="padding: 10px 20px">
+                        <?= $_SESSION['INVALID_QUESTION'] ?>
                     </div>
+
+                    <?php unset($_SESSION['INVALID_QUESTION']); ?>
+                <?php endif; ?>
                 </div>
-            <div class="d-flex text-center mt-3 " style="margin: 0px 90px;">
-                <button type="button" class="mx-3 btn custom-btn-secondary text-white mt-1" style="width: 225px" onclick="location.href = 'create1.php'">Back</button>
-                <button type="submit" class="mx-3 btn custom-btn-primary text-white mt-1" name="create" style="width: 225px">Create</button>
-            </div>
+
+                <section class="text-center mt-3">
+                        <button name="create" class="mb-3 rounded-md px-2 py-2 bg-[#1D70A2] border-[#1D70A2] hover:bg-[#185f89] hover:border-[#185f89] border w-2/3 mx-auto">
+                            Continue
+                        </button>
+
+                    <a href="index.php">
+                        <div class="mx-auto w-2/3 bg-[#5D737E] py-2 rounded-md text-center hover:bg-[#546771] select-none">
+                        Back
+                        </div>
+                    </a>
+                </section>
         </form>
     </div>
 </body>
