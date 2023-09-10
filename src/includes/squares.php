f157a632a31800189f1a4e3f6175598039811f7c
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-09 20:35:20 -0400

    Increase transparency

--- src/includes/squares.php
+++ src/includes/squares.php
@@ -1,13 +1,13 @@
 <section class="overflow-hidden z-[-1]">
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[200px] w-[200px]  top-[-50px] left-[200px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[200px] w-[200px]  top-[170px] left-[70px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[150px] w-[150px]  top-[400px] left-[-20px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[300px] w-[300px] top-[-100px] left-[-200px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[120px] w-[120px]  top-[400px] left-[240px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[200px] w-[200px]  top-[-50px] left-[200px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[200px] w-[200px]  top-[170px] left-[70px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[150px] w-[150px]  top-[400px] left-[-20px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[300px] w-[300px] top-[-100px] left-[-200px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[120px] w-[120px]  top-[400px] left-[240px]"></div>
 
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[300px] w-[300px] top-[-150px] right-[100px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[120px] w-[120px]  top-[130px] right-[30px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[100px] w-[100px]  top-[220px] right-[280px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[250px] w-[250px]  top-[270px] right-[50px]"></div>
-  <div class="border-[0.5px] border-[#6daedb44] absolute rotate-[30deg] rounded-xl h-[100px] w-[100px]  top-[490px] right-[295px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[300px] w-[300px] top-[-150px] right-[100px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[120px] w-[120px]  top-[130px] right-[30px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[100px] w-[100px]  top-[220px] right-[280px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[250px] w-[250px]  top-[270px] right-[50px]"></div>
+  <div class="border-[0.5px] border-[#6daedb33] absolute rotate-[30deg] rounded-xl h-[100px] w-[100px]  top-[490px] right-[295px]"></div>
 </section>
